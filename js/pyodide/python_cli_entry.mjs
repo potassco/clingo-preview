c64d76ce1f0b0306886772cfb09977291aa4cab4
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-03-24 03:19:05 +0100

    update pyodide

--- js/pyodide/python_cli_entry.mjs
+++ js/pyodide/python_cli_entry.mjs
@@ -9,10 +9,16 @@ import { readdirSync } from "fs";
  * names it is possible this could break. The most surprising one here is tmp, I
  * am not sure why but if we link tmp then the process silently fails.
  */
-function dirsToMount() {
+function rootDirsToMount() {
+  const skipDirs = ["dev", "lib", "proc", "tmp"];
   return readdirSync("/")
-    .filter((dir) => !["dev", "lib", "proc"].includes(dir))
+    .filter((dir) => !skipDirs.includes(dir))
     .map((dir) => "/" + dir);
+}
+
+function dirsToMount() {
+  const extra_mounts = process.env["_PYODIDE_EXTRA_MOUNTS"] || "";
+  return rootDirsToMount().concat(extra_mounts.split(":").filter((s) => s));
 }
 
 const thisProgramFlag = "--this-program=";
