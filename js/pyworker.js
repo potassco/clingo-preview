7ffc7c3d8b1046c6abefe2211e8cc37c4c2ece7f
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-04-16 00:01:38 +0200

    update clingo module

--- js/pyworker.js
+++ js/pyworker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.27.4/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.27.5/full/pyodide.js");
 
 const messageSchemas = {
     run: {
